68fefd8db3e1884af732de114b54bf858174f7ec
Your Name <you@example.com>
2013-10-21 05:54:00 -0700

    fixed paths

--- 98-rootfs/build.sh
+++ 98-rootfs/build.sh
@@ -14,11 +14,11 @@ CURRENT_SUBDIR_PATH=$(pwd)
 # Building rootfs
 #
 mkdir SOURCE
-cp ../04-android/OUTPUT/root/* SOURCE/
-cp ../04-android/OUTPUT/system SOURCE/
+cp -r ../04-android/OUTPUT/root/* SOURCE/
+cp -r ../04-android/OUTPUT/system SOURCE/
 cp ../03-busybox/OUTPUT/busybox SOURCE/system/xbin/
 cp ../05-serialtest/OUTPUT/SerialTest.apk SOURCE/system/app/
-mkdir -p system/lib/modules
+mkdir -p SOURCE/system/lib/modules
 find ../02-kernel/OUTPUT/lib/modules -name \*.ko -exec cp {} SOURCE/system/lib/modules/ \;
 cp -r ../02-kernel/OUTPUT/lib/firmware SOURCE/system/lib/
 
@@ -26,7 +26,7 @@ cp -r ../02-kernel/OUTPUT/lib/firmware SOURCE/system/lib/
 # Create tarball
 #
 mkdir OUTPUT
-sudo ../04-android/build/tools/mktarball.sh ../04-android/out/host/linux-x86/bin/fs_get_stats SOURCE/ . rootfs ../OUTPUT/rootfs.tar.bz2
+sudo ../04-android/SOURCE/build/tools/mktarball.sh ../04-android/SOURCE/out/host/linux-x86/bin/fs_get_stats SOURCE/ . rootfs OUTPUT/rootfs.tar.bz2
 
 #
 # If successfull, create an empty file
